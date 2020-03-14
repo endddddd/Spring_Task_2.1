0b78b29897c4d3f87274352b5e5f1173549860ba
endddddd <endddddd1@gmail.com>
2020-03-14 22:36:47 +0300

    update

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +29,14 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public User getUserByCar(String name, int series) {
+
         try {
             TypedQuery<User> query = sessionFactory.getCurrentSession()
                     .createQuery("from User where car.series =:pSeries and car.name =:pName")
                     .setParameter("pSeries", series)
                     .setParameter("pName", name);
-            return query.getSingleResult();
+            return query.getResultList().get(0);
+
         } catch (Exception ex) {
             ex.getMessage();
         }
